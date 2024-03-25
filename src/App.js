9e571c5dee6684b28c115970029df7aea21cf495
Jagdishchandradive <jagdishchandradive@gmail.com>
2024-03-26 01:08:36 +0530

    Route Path Added for 'Layout'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import Layout from './components/Layout';
 import api from './api/axiosConfig';
+import {Routes, Route} from 'react-router-dom';
 import { useState,useEffect } from 'react';
+import Home from './components/home/Home';
 
 function App() {
   const [movies,setMovies]=useState();
 
   const getMovies=async () =>{
     try{
-      const response=await api.get("./api/v1/movies");
+      const response=await api.get("http://localhost:8080/api/v1/movies");
       console.log(response.data);
       setMovies(response.data);
-
     }
     catch(err){
       console.log(err);
@@ -25,7 +28,11 @@ function App() {
 
   return (
     <div className="App">
-    
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route path="/" element={<Home movies={movies} />} ></Route>
+      </Route>
+    </Routes>
     </div>
   );
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:'',
+    baseURL:'https://d39b-27-4-199-131.ngrok-free.app',
     headers:{"ngrok-skip-browser-warning":"true"}
 });
