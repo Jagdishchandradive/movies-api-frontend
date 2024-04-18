1358360879307680fa8eaef3c252e57062f5738c
Jagdishchandradive <jagdishchandradive@gmail.com>
2024-04-18 17:34:13 +0530

    base URL added

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
     baseURL:'https://movies-api-backend-production.up.railway.app/',
-    headers:{"ngrok-skip-browser-warning":"true"}
+   
 });
