4771a51c19c145f769e81a87bf6681e7309970e8
Jagdishchandradive <jagdishchandradive@gmail.com>
2024-04-25 16:31:27 +0530

    localhost backend enpoint check

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [reviews,setReviews]=useState();
   const getMovies=async () =>{
     try{
-      const response=await api.get("https://localhost:8080/");
+      const response=await api.get("http://localhost:8080/api/v1/movies");
       console.log(response.data);
       setMovies(response.data);
     }
@@ -26,7 +26,7 @@ function App() {
      
     try 
     {
-        const response = await api.get(`https://movies-api-backend-production.up.railway.app/${movieId}`);
+        const response = await api.get(`http://localhost:8080/api/v1/movies/${movieId}`);
 
         const singleMovie = response.data;
 
